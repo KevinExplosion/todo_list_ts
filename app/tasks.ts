de8865551dcf91a18f22c752150b37813d525e6b
david remington and kevin mattison <student@epicodus.com>
2016-03-21 11:45:09 -0700

    I hate you, TypeScript.

--- app/tasks.ts
+++ app/tasks.ts
@@ -1,16 +1,43 @@
 class Task {
-  done: boolean;
-  constructor(public description: string, public priority: string){
-    this.done = false;
-  }
+  done: boolean = false;
+  constructor(public description: string, public priority: string){}
   markDone(){
     this.done = true;
   }
 }
+class HomeTask extends Task {}
+//First, we have added this line: class HomeTask extends Task {} By using the keyword extends, we are saying that the class HomeTask is inheriting from the base class Task.
+
+class HobbyTask extends Task {
+  constructor(public description: string) {
+    super(description, "low");
+  }
+}
+
+class WorkTask extends Task {
+  constructor(public dueDate: Date, public description: string, public priority: string) {
+    super(description, priority);
+  }
+}
 
 var tasks = [];
-tasks.push(new Task("Do the dishes.", "High"));
-tasks.push(new Task("Buy chocolate.", "Low"));
-tasks.push(new Task("Wash the laundry.", "High"));
-tasks[0].markDone();
+tasks.push(new HomeTask("Do the dishes.", "High"));
+tasks.push(new HomeTask("Buy chocolate.", "Low"));
+tasks.push(new HomeTask("Wash the laundry.", "High"));
+
+tasks.push(new HobbyTask("Practice origami."));
+tasks.push(new HobbyTask("Bake a pie."));
+
+var today = new Date();
+var tomorrow = new Date();
+tomorrow.setDate(today.getDate() + 1);
+var nextDay = new Date();
+nextDay.setDate(today.getDate() + 2);
+
+tasks.push(new WorkTask(today, "Update blog.", "High"));
+tasks.push(new WorkTask(tomorrow, "Go to meeting.", "Medium"));
+tasks.push(new WorkTask(nextDay, "Clean ceiling.", "Low"));
+
 console.log(tasks);
+
+/* zippidy doda */
